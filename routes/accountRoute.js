653f69aea5c6642561bd123bee3b5d994b280851
GateK33p3r363 <167857743+GateK33p3r363@users.noreply.github.com>
2024-05-13 13:57:39 +0200

    Week 4 Activity changes

---
+++ routes/accountRoute.js
@@ -0,0 +1,26 @@
+// Import necessary modules
+const express = require('express');
+const router = express.Router();
+const { getAccountInfo } = require('../controllers/accountController');
+const { someUtilityFunction } = require('../utilities/index');
+const regValidate = require('../utilities/account-validation')
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
+// Define routes
+router.get('/', (req, res) => {
+    // Handle the GET request for the "My Account" link
+    const accountInfo = getAccountInfo();
+    const utilityResult = someUtilityFunction();
+    res.send(`Account Information: ${accountInfo}. Utility Result: ${utilityResult}`);
+    router.post('/register', utilities.handleErrors(accountController.registerAccount))
+});
+
+// Export the router
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,34 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
@@ -26,6 +54,10 @@ app.use(static)
 // Index route
 app.get("/", baseController.buildHome)
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", require("./routes/accountRoute"))
+// Route to build login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
 /* ***********************
  * Local Server Information
